7864e7fca97d60b00d04b7af6326b8d784d59365
Muhammadalii9 <kingromance225@gmail.com>
2026-05-09 01:59:39 +0500

    Refactor app.py for improved structure and clarity

--- app.py
+++ app.py
@@ -45,13 +45,13 @@ st.divider()
 st.header("👖 Shalwar Details")
 s1, s2, s3, s4 = st.columns(4)
 with s1:
-    sl = st.number_input("Shalwar Lambai", value=36.0, step=0.5)
-with s2:
-    sw = st.number_input("Shalwar Loosing", value=16.0, step=0.5)
-with s3:
-    sp = st.number_input("Paicha", value=7.5, step=0.5)
-with s4:
     sp = st.text_input("shalwar")
+with s2:
+    sl = st.number_input("Shalwar Lambai", value=36.0, step=0.5)
+with s3:
+    sw = st.number_input("Shalwar Loosing", value=16.0, step=0.5)
+with s4:
+    sp = st.number_input("Paicha", value=7.5, step=0.5)
     
 st.divider()
 
